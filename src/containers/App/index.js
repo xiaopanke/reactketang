751cc4a4e4ee9bb1c4a04733f165e8076b5cb0e2
xiaopanke <1547947237@qq.com>
2017-11-14 11:47:30 +0800

    加个基本的文件

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,13 +4,18 @@ import {
   Route,
   NavLink
 } from 'react-router-dom'
-import './index.less'
+import './index.less';
+import Home from '../Home/index';
+import Courses from '../Courses/index';
+import Profile from '../Profile/index';
 export default class App extends Component{
   render(){
     return (
       <Router>
         <div>
-          <Route></Route>
+          <Route path="/" component={Home} />
+          <Route path="/courses" component={Courses} />
+          <Route path="/profile" component={Profile} />
         </div>
       </Router>
     )
---
+++ src/containers/Courses/index.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import './index.less'
+export default class Course extends Component{
+  render(){
+    return (
+      <div>course</div>
+    )
+  }
+}
