0dbc9c371a3dac1987806b799f71e8f9de5470b5
xiaopanke <1547947237@qq.com>
2017-11-14 12:29:40 +0800

    底部导航

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -8,11 +8,13 @@ import './index.less';
 import Home from '../Home/index';
 import Courses from '../Courses/index';
 import Profile from '../Profile/index';
+import Tab from '../../components/Tab/index'
 export default class App extends Component{
   render(){
     return (
       <Router>
         <div>
+          <Tab />
           <Route exact path="/" component={Home} />
           <Route path="/courses" component={Courses} />
           <Route path="/profile" component={Profile} />
